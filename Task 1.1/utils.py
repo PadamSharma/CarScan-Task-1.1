39f2815d2178a091995088a2aca1cf1f635fe5d2
Padam Sharma <56642812+PadamSharma@users.noreply.github.com>
2022-04-09 16:49:54 +0530

    Update utils.py

--- Task 1.1/utils.py
+++ Task 1.1/utils.py
@@ -14,11 +14,6 @@ from matplotlib.pyplot import imshow
 from matplotlib import cm
 import random
 
-data_dirs = sorted(glob("./Assignment/Data Visualization/data/*"))
-
-img_dirs = sorted(glob("./Assignment/Data Visualization/images/*"))
-js1 = pd.read_json(data_dirs[0])
-
 #  UTILITY FUNCTIONS
 
 
